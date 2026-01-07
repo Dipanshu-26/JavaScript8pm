f46d8949ba9132875d464d9a4321970d4f980269
Dipanshu-26 <dnchawde@gmail.com>
2026-01-07 20:42:16 +0530

    7jan

--- 15-arrayfuncn.js
+++ 15-arrayfuncn.js
@@ -76,3 +76,58 @@ let withdrow = transaction.filter(function(el){
 })
 console.log(`withdrow = ${withdrow}`)
 console.log(withdrow)
+
+console.log("----------------------------------------")
+// reduce(function(acc, el, index, arr), initialValue)
+// acc => accumulator (acts as a temporary variable)
+// action => reduces all elements to a single value
+// return => single value (number / string / object)
+
+//         0  1  2  3  4  5
+// let arr= [11,22,33,44,55,66]
+// let total = 0
+// for(let i=0;i<arr.length;i++){     //i=0 , 1  ,2  total=0,11,33....
+//     total = total + arr[i]         //total = 0 + 11 =11,  total = 11+22= 33, total = 33+33 =66 .....
+// }
+
+// console.log(total)
+
+let arr= [11,22,33,44,55,66]
+
+let addn = arr.reduce(function(acc,el,inx,arr){
+        return el+acc
+},0)
+
+console.log(addn)
+
+
+//forEach()
+// Welcome, Dipanshu !!!
+//               0        1      2       3          last index = 3   but length= 4
+// let names = ['dipanshu','neel','nitin','tanish']     // i<names.lenth  (i<4)  =3    ===> i<=names.length-1  (i<=4-1)
+
+// for(let i =0;i<=names.length-1;i++){
+//     console.log(`Welcome, ${names[i]} !!!`)
+// }
+
+let names = ['dipanshu','neel','nitin','tanish']
+
+names.forEach(function(el,inx,arr){       //el = 'dipanshu','neel','nitin','tanish'
+    console.log(`Welcome, ${el} !!!`)
+})
+
+//object 
+// properties
+// methods 
+// action
+// return 
+
+// Array 
+// methods 
+// action
+// return 
+
+// person 
+// properties : name,height,weight
+// methods :walk(), talk()
+// return : healthyweight, motivation , decision
