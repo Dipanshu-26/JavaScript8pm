f9f6fb093d3e85a3e6c7352dd5438c90ab526db3
Dipanshu-26 <dnchawde@gmail.com>
2026-03-18 19:41:33 +0530

    18-mar

--- AJAX/APIcall2/script.js
+++ AJAX/APIcall2/script.js
@@ -1,32 +1,69 @@
-function getUserInfo(pageno){
-    fetch(`https://reqres.in/api/users?page=${pageno}`,{
-        method : 'GET',
-        headers : {
-            "x-api-key" : "reqres_7719223fd6664035a09558d2a98b01d7"
+function getUserInfo(pageno) {
+    return fetch(`https://reqres.in/api/users?page=${pageno}`, {
+        method: 'GET',
+        headers: {
+            "x-api-key": "reqres_7719223fd6664035a09558d2a98b01d7"
         }
-    }).then(function(res){
+    }).then(function (res) {
         //console.log(res)
         return res.json()
-    }).then(function(res2){
-        console.log(res2)
+    }).then(function (res2) {
+        //console.log(res2)
+        return res2
     })
 }
 
-getUserInfo(2)
+//getUserInfo(2)
 
 //------------------------------------------------------------------
 
-function getSingleUser(userId){
-    fetch(`https://reqres.in/api/users/${userId}`,{
-        method:"GET",
-        headers:{
-             "x-api-key" : "reqres_7719223fd6664035a09558d2a98b01d7"
+function getSingleUser(userId) {
+    return fetch(`https://reqres.in/api/users/${userId}`, {
+        method: "GET",
+        headers: {
+            "x-api-key": "reqres_7719223fd6664035a09558d2a98b01d7"
         }
-    }).then(function(res){
+    }).then(function (res) {
         return res.json()
-    }).then(function(res2){
-        console.log(res2)
+    }).then(function (res2) {
+        //console.log(res2)
+        return res2
     })
 }
 console.log("-----------------------------")
-getSingleUser(3)
+//getSingleUser(3)
+//-----------------------------------------------------------------------------------
+function renderHTML(el) {
+    document.writeln(`<h1>${el.first_name}  ${el.last_name}</h1> <br>`)
+    document.writeln(`<h1>${el.id}</h1> <br>`)
+    document.writeln(`<h1>${el.email}</h1> <br>`)
+    document.writeln(`<img src= ${el.avatar}>`)
+}
+
+
+
+
+
+
+//-------------------------------------------------------------------------------------
+// getUserInfo(1).then(function (userList) {
+//     //console.log(userList)
+//     let id = userList.data[0].id
+//     //console.log(id)
+//     return getSingleUser(id)
+// }).then(function (singleUser) {
+//     //console.log(singleUser)
+//     //console.log(singleUser.data)
+//     renderHTML(singleUser.data)
+// })
+
+//--------------------------------------------------------------------------------------
+
+async function getInfo(pageno){
+    let userData = await getUserInfo(pageno)
+    let userId = userData.data[0].id
+    let singleUser = await getSingleUser(userId)
+    await renderHTML(singleUser.data)
+}
+
+getInfo(2)
