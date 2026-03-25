c95b542fc07c4d55c43774785d10746d6f7636a3
Dipanshu-26 <dnchawde@gmail.com>
2026-03-25 19:23:06 +0530

    25-mar

--- AJAX/APIcall3/script22.js
+++ AJAX/APIcall3/script22.js
@@ -8,82 +8,86 @@
 //will execute promises till it get first reject ... 
 //after getting first reject it will through shortcircut
 
-// async function loadDashboard() {
-//     //try {
-//         const [page1, page2, page3] = await Promise.all([
-//             fetch('https://reqres.in/api/users?page=1', {
-//                 method: 'GET',
-//                 Headers: {
-//                     "x-api-key": "reqres_7719223fd6664035a09558d2a98b01d7"
-//                 }
-//             }).then(res => res.json()),
+async function loadDashboard() {
+    try {
+        const [page1, page2, page3] = await Promise.all([
+            fetch('https://reqres.in/api/users?page=1', {
+                method: 'GET',
+                headers: {
+                    "x-api-key": "reqres_7719223fd6664035a09558d2a98b01d7"
+                }
+            }).then(res => res.json()),
             
 
-//             fetch('https://reqres.in/api/users?page=2', {
-//                 method: 'GET',
-//                 headers: {
-//                     "x-api-key": "reqres_7719223fd6664035a09558d2a98b01d7"
-//                 }
-//             }).then(res => res.json()),
+            fetch('https://reqres.in/api/users?page=2', {
+                method: 'GET',
+                headers: {
+                    "x-api-key": "reqres_7719223fd6664035a09558d2a98b01d7"
+                }
+            }).then(res => res.json()),
 
-//             fetch('https://reqres.in/api/users?page=1', {
-//                 method: 'GET',
-//                 headers: {
-//                     "x-api-key": "reqres_7719223fd6664035a09558d2a98b01d7"
-//                 }
-//             }).then(res => res.json())
-//         ])
+            fetch('https://reqres.in/api/users?page=3', {
+                method: 'GET',
+                headers: {
+                    "x-api-key": "reqres_7719223fd6664035a09558d2a98b01d7"
+                }
+            }).then(res => res.json())
+        ])
 
-//         console.log(page1.data)
-//   //  } catch (error){
-//         //console.log('Dashboard failed to load', error )
-//    // }
-// }
-
-async function loadDashboard() {
-  try {
-    const [users, page1, page2] = await Promise.all([
-      fetch('https://reqres.in/api/users?page=1', {
-        method: 'GET',
-        headers: {
-          "x-api-key": "reqres_7719223fd6664035a09558d2a98b01d7"
-        }
-      }).then(r => r.json()),
-      fetch('https://reqres.in/api/users?page=2', {
-        method: 'GET',
-        headers: {
-          "x-api-key": "reqres_7719223fd6664035a09558d2a98b01d7"
-        }
-      }).then(r => r.json()),
-      fetch('https://reqres.in/api/users?page=3', {                //no data [] on page 3
-        method: 'GET',
-        headers: {
-          "x-api-key": "reqres_7719223fd6664035a09558d2a98b01d7"
-        }
-      }).then(r => r.json())
-    ])
-    //console.log(`user data = ${users.data}, page1 data = ${page1.data}, page 2 data = ${page2.data}`)
-    console.log(users.data, page1.data, page2.data)
-  } catch (error) {
-    console.error('Dashboard failed to load', error)
-  }
+        console.log(page1.data,page2.data, page3.data)
+        //console.log(page3.data)
+   } catch (error){
+        console.log('Dashboard failed to load', error )
+   }
 }
-
-//function call
-loadDashboard()
-
-
-
-
-
-
-
-
-
-
+//loadDashboard()
 
 //promise.any()
 //it will exectue till it get first resolve
+
+async function loadDashboard2() {
+  try{
+    let response = await Promise.any([
+        fetch('https://wrong-url.com/api', {
+                method: 'GET',
+                headers: {
+                    "x-api-key": "reqres_7719223fd6664035a09558d2a98b01d7"
+                }
+            }),
+            
+            fetch('https://wrong-url.com/api', {
+                method: 'GET',
+                headers: {
+                    "x-api-key": "reqres_7719223fd6664035a09558d2a98b01d7"
+                }
+            }),
+
+            fetch('https://reqres.in/api/users?page=1', {
+                method: 'GET',
+                headers: {
+                    "x-api-key": "reqres_7719223fd6664035a09558d2a98b01d7"
+                }
+            })
+    ])
+   const data = await response.json()
+   console.log(data.data)
+  } catch(error){
+    console.log(`error == ${error}`)
+  }
+
+}
+loadDashboard2()
+
+//.race .allsettle
+
+
+
+
+
+
+
+
+
 
 //promise.allsettled()
 //it will execute all and return whatever is result in array..
